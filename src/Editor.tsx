903efbafa227185e1035b2f8cef7a86559c4f6df
Prachi-2002 <prachimadame01@gmail.com>
2025-02-05 10:30:31 +0530

    fix: fixed eslint issue

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import { $getSelection, $isRangeSelection } from 'lexical';
 import { AutoFocusPlugin } from '@lexical/react/LexicalAutoFocusPlugin';
